e955b9a0f081037be28f39c5e04e89468cbb2d0f
신동석 <96603945+dss1222@users.noreply.github.com>
2022-01-15 10:09:58 +0900

    문제 9~17 추가

---
+++ src/main/java/solution18.java
@@ -0,0 +1,13 @@
+public class solution18 {
+    public static void main(String[] args) {
+        String[] seoul = {"Jane","Kim"};
+        String answer = "";
+
+        for(int i = 0; i < seoul.length; i++){
+            if(seoul[i].equals("Kim")){
+                answer = "김서방은 " + i + "에 있다";
+                break;
+            }
+        }
+    }
+}
