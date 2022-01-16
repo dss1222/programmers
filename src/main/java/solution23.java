ba2b54f0380b9791c0fb4a30ccecb7c0770c1ad7
신동석 <96603945+dss1222@users.noreply.github.com>
2022-01-16 18:45:54 +0900

    23번 추가

---
+++ src/main/java/solution23.java
@@ -0,0 +1,11 @@
+public class solution23 {
+    public static void main(String[] args) {
+        long n =12345;
+        String tempStr = new tempStr.toCharArray();
+        char[] tempChar = tempStr.toCharArray();
+        int[] answer = new int[tempChar.length];
+        for(int i=0; i<tempChar.length;i++){
+            answer[i] = Integer.parseInt(tempChar[tempChar.length-1-i]+"");
+        }
+    }
+}
