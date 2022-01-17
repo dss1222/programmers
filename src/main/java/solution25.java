37b96a69004c73233e6ec6a79e7f67d0685c19db
신동석 <96603945+dss1222@users.noreply.github.com>
2022-01-17 16:01:20 +0900

    25번 추가

--- src/main/java/solution25.java
+++ src/main/java/solution25.java
@@ -1,19 +1,13 @@
 import java.util.*;
-import java.io.*;
 public class solution25 {
     public static void main(String[] args) {
-        long n = 118372;
-        String s = String.valueOf(n);
-        String ansnum= "";
-        long answer;
-        char[] arr = new char[s.length()];
-        for(int i=0; i<s.length(); i++){
-            arr[i] += s.charAt(i);
-        }
-        Arrays.sort(arr);
-        for(int i=arr.length-1; i>=0; i--){
-            ansnum += arr[i];
-        }
-        System.out.println(ansnum);
+       long n = 121;
+       long answer = 0;
+       Double sqrt = Math.sqrt(n);
+       if(sqrt == sqrt.intValue()){
+           answer = (long)Math.pow(sqrt +1, 2);
+       }else answer =  -1;
+
+        System.out.println(answer);
     }
 }
