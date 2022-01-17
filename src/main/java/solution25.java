eb47b6f9caef8bf2dbc5efc12902f93774f3436f
신동석 <96603945+dss1222@users.noreply.github.com>
2022-01-17 15:25:25 +0900

    25번 추가

---
+++ src/main/java/solution25.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+public class solution25 {
+    public static void main(String[] args) {
+        long n = 118372;
+        String s = String.valueOf(n);
+        String ansnum= "";
+        long answer;
+        char[] arr = new char[s.length()];
+        for(int i=0; i<s.length(); i++){
+            arr[i] += s.charAt(i);
+        }
+        Arrays.sort(arr);
+        for(int i=arr.length-1; i>=0; i--){
+            ansnum += arr[i];
+        }
+        System.out.println(ansnum);
+    }
+}
